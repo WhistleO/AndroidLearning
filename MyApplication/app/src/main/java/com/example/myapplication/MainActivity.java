1f6657463c688585273b3c1d6c191b56fdd8afe2
WhistleO <WhistleO@outlook.com>
2019-08-30 12:47:35 +0800

    按照要求，完成课堂练习：写一个可以点击按钮拨打电话的应用。 主要修改了： 1. MainActivity.java: 这个是java代码，用于动态。 2. AndroidMainfest.xml: 在这里需要给app打电话权限，就一行语句。在调试时需要给那个app权限才行。 3. strings.xml 按钮的文字，用类似变量方式。 4. activity_main.xml:界面设置。首先改了线性布局方式，然后增加了文本和按钮。

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"执行了onCreate()方法");
+
+        Button button =(Button)findViewById(R.id.btn_finish);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent =new Intent();
+                intent.setAction(intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:18205847410"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"执行了onStart()方法");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"执行了onResume()方法");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"执行了onStop()方法");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"执行了onPause()方法");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"执行了onRestart()方法");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"执行了onDestroy()方法");
     }
 }
